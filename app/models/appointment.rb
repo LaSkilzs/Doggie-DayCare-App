338a5d5d099ad973dec48d4f83b92969f1c8b7a0
La Fountain <github email address>
2019-04-08 19:06:52 -0400

    Fix ui sheets

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :dog
   belongs_to :walker
   belongs_to :service
+  has_many :invoices
   enum status: {pending: "pending", confirmed: "confirmed", cancelled: "cancelled", completed: "completed"}
 
   validates_presence_of :date, :start_time, :status
