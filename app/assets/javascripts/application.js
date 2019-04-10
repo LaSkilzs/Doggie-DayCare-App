2ee3956fa9ad93d7604b01b5c762316022ca620f
La Fountain <github email address>
2019-04-10 19:35:37 -0400

    Add front page, switch navbar, changed appointment, invoice page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
+//= require jquery3
+//= require rails-ujs
+//= require turbolinks
 //= require bootstrap-sprockets
+//= require_tree .
